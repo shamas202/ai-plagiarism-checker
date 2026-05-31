dea88025c5f52da1a70bf39ea0dadb39773e67ca
shamas202 <shamsiop7000@gmail.com>
2026-06-01 01:31:26 +0500

    Cleaning up whitespace and indentations (refinement 7)

--- frontend/postcss.config.js
+++ frontend/postcss.config.js
@@ -13,3 +13,4 @@ export default {
 # Refinement 271: Standardizing code style and formatting
 # Refinement 342: Minor refactoring of function calls
 # Refinement 441: Standardizing code style and formatting
+# Refinement 7: Cleaning up whitespace and indentations

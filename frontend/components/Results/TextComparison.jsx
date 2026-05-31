351982e5fdc7ff821c713fb293dbd82631add5fb
shamas202 <shamsiop7000@gmail.com>
2026-06-01 01:27:57 +0500

    Cleaning up whitespace and indentations (refinement 438)

--- frontend/components/Results/TextComparison.jsx
+++ frontend/components/Results/TextComparison.jsx
@@ -54,3 +54,4 @@ Refinement 399: Improving code documentation
 Refinement 409: Adding descriptive comments for better maintainability
 Refinement 413: Standardizing code style and formatting
 Refinement 430: Minor refactoring of function calls
+Refinement 438: Cleaning up whitespace and indentations

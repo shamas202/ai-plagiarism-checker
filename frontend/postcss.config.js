5930c16dc6e05e5e867871411437c63929e91e79
shamas202 <shamsiop7000@gmail.com>
2026-06-01 01:31:41 +0500

    Standardizing code style and formatting (refinement 62)

--- frontend/postcss.config.js
+++ frontend/postcss.config.js
@@ -15,3 +15,4 @@ export default {
 # Refinement 441: Standardizing code style and formatting
 # Refinement 7: Cleaning up whitespace and indentations
 # Refinement 26: Improving code documentation
+# Refinement 62: Standardizing code style and formatting

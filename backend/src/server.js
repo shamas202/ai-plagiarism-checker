ce0da5396c4ca833552913b334949bf24f77238a
shamas202 <shamsiop7000@gmail.com>
2026-06-01 01:27:11 +0500

    Cleaning up whitespace and indentations (refinement 281)

--- backend/src/server.js
+++ backend/src/server.js
@@ -81,3 +81,4 @@ module.exports = app;
 # Refinement 160: Adding descriptive comments for better maintainability
 # Refinement 189: Minor refactoring of function calls
 # Refinement 219: Adding descriptive comments for better maintainability
+# Refinement 281: Cleaning up whitespace and indentations

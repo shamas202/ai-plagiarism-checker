6fd78b9b913215bb1c3099bad4b490e3fb0ccc1c
shamas202 <shamsiop7000@gmail.com>
2026-06-01 01:26:17 +0500

    Cleaning up whitespace and indentations (refinement 128)

--- backend/src/utils/logger.js
+++ backend/src/utils/logger.js
@@ -19,3 +19,4 @@ const logger = winston.createLogger({
 module.exports = logger;
 # Refinement 92: Minor refactoring of function calls
 # Refinement 117: Updating documentation for future reference
+# Refinement 128: Cleaning up whitespace and indentations

83f93eb1125e492b9b395e7efe4cdc1fec0db358
shamas202 <shamsiop7000@gmail.com>
2026-06-01 01:28:03 +0500

    Minor refactoring of function calls (refinement 454)

--- frontend/components/Results/ScoreCard.jsx
+++ frontend/components/Results/ScoreCard.jsx
@@ -71,3 +71,4 @@ Refinement 223: Optimizing logic in small sections
 Refinement 288: Refining variable names for clarity
 Refinement 310: Cleaning up whitespace and indentations
 Refinement 349: Minor refactoring of function calls
+Refinement 454: Minor refactoring of function calls
